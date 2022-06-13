d621ec293294430dbc379c0aff1a6fc335a5f9e3
Luisda <lsanchezroldan@gmail.com>
2022-06-13 22:24:20 +0200

    Definición de taclas de juego

--- pong.py
+++ pong.py
@@ -1,4 +1,8 @@
+from ssl import ALERT_DESCRIPTION_BAD_CERTIFICATE_HASH_VALUE
 import pygame
+
+ANCHO_PALETA = 5
+ALTO_PALETA = 40
 
 """
  -algo de herencia:
@@ -13,7 +17,27 @@ import pygame
 """
 
 class Paleta(pygame.Rect):
-    pass    
+
+    ARRIBA = True
+    ABAJO = False
+
+    def __init__(self, x, y):
+        super(Paleta, self).__init__(x, y, ANCHO_PALETA, ALTO_PALETA)
+        self.velocidad = 5
+
+    def muevete(self, direccion):
+        if direccion == self.ARRIBA:
+            print("Muevete arriba")
+        else:
+            print("Muevete abajo") 
+
+"""
+-el movimiento es cosa de la paleta
+-aumentar o disminuir el valor del eje y posición de la paleta
+-quien tiene que capturar el evento de pulsar la tecla es el cucle principal
+redibujar la paleta
+
+"""
 
 class Pong:
 
@@ -21,8 +45,7 @@ class Pong:
     _ANCHO = 640
     _MARGEN_LATERAL = 40
 
-    _ANCHO_PALETA = 10
-    _ALTO_PALETA = 40
+
 
     def __init__(self):
         print("Construyendo un objeto pong")
@@ -31,15 +54,13 @@ class Pong:
 
         self.jugador1 = Paleta(
             self._MARGEN_LATERAL,                     #coordenada x (left)
-            (self._ALTO-self._ALTO_PALETA)/2,         #coordenada y (top)   
-            self._ANCHO_PALETA,                       #ancho (width)
-            self._ALTO_PALETA)                        #alto (height)
+            (self._ALTO-ALTO_PALETA)/2)         #coordenada y (top)   
+            
 
         self.jugador2 = Paleta(
-            self._ANCHO-self._MARGEN_LATERAL-self._ANCHO_PALETA,
-            (self._ALTO-self._ALTO_PALETA)/2,
-            self._ANCHO_PALETA,
-            self._ALTO_PALETA)
+            self._ANCHO-self._MARGEN_LATERAL-ANCHO_PALETA,
+            (self._ALTO-ALTO_PALETA)/2)
+        
 
         
 
@@ -49,14 +70,24 @@ class Pong:
             for evento in pygame.event.get():
                 if evento.type == pygame.KEYDOWN:
                     if evento.key == pygame.K_ESCAPE:
-                        return
+                        salir = True
+                    elif evento.key == pygame.K_a:
+                        self.jugador1.muevete(Paleta.ARRIBA)
+                    elif evento.key == pygame.K_z:
+                        self.jugador1.muevete(Paleta.ABAJO)
+                    elif evento.key == pygame.K_k:
+                        self.jugador2.muevete(Paleta.ARRIBA)
+                    elif evento.key == pygame.K_m:
+                        self.jugador1.muevete(Paleta.ABAJO)
+
+
                 if evento.type == pygame.QUIT:
                     return
 
-            for position in range(0, self._ALTO, 40):
-                color = (255, 255, 255)        
-                pygame.draw.line(self.pantalla, color, (self._ANCHO/2, position), (self._ANCHO/2, position +10))
-                # pygame.draw.line(self.pantalla, (255, 0, 0), (self._ANCHO/2, 0), (self._ANCHO/2, self._ALTO))        
+            # for position in range(0, self._ALTO, 40):     #linea discontinua
+                # color = (255, 255, 255)        
+                # pygame.draw.line(self.pantalla, color, (self._ANCHO/2, position), (self._ANCHO/2, position +10))
+            pygame.draw.line(self.pantalla, (255, 0, 0), (self._ANCHO/2, 0), (self._ANCHO/2, self._ALTO))        
             pygame.draw.rect(self.pantalla, (0, 255, 0), self.jugador1)
             pygame.draw.rect(self.pantalla, (0, 0, 255), self.jugador2)
             pygame.display.flip()
